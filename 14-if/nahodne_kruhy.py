caf09e64a9a464b7274470e24ddbd4191d39c570
Alex Trunkvalter <alex.why.would.you@gmail.com>
2023-12-20 10:40:08 +0100

    new nahodne_kruhy.py

---
+++ 14-if/nahodne_kruhy.py
@@ -0,0 +1,24 @@
+import tkinter, random
+
+canvasWidth = 300
+canvasHeight = 300
+canvas = tkinter.Canvas(width=canvasWidth,height=canvasHeight)
+canvas.pack()
+
+polomer = 10
+pocetKruhov = 300
+okraj = 0
+for i in range(pocetKruhov):
+    x = random.randint(polomer+2,canvasWidth -polomer-2)
+    y = random.randint(polomer+2,canvasHeight - polomer-2)
+    if x <= canvasWidth//2 and y<=canvasHeight //2:
+        farba = 'red'
+    elif x >= canvasWidth//2 and y <= canvasHeight//2:
+        farba = 'blue'
+    elif x <= canvasWidth//2 and y>= canvasHeight//2:
+        farba = 'yellow'
+    else:
+        farba = 'green'
+    canvas.create_oval(x-polomer,y-polomer,x+polomer,y+polomer,fill=farba,width = okraj)
+
+tkinter.mainloop()

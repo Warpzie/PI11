1ccbd3ecf32247ba6ac12e0f47ad91e505202108
trunkvaltera23 <alex.why.would.you@gmail.com>
2024-04-10 09:48:31 +0200

    continued 1

--- 20-29/20-string/1.py
+++ 20-29/20-string/1.py
@@ -1,5 +1,21 @@
 retazec = 'america ya'
 print(retazec)
-for i in range(len(retazec)):
-    print(i,retazec[i])
+print(retazec[-1])
+
+#for i in range(len(retazec)):
+#    print(i,retazec[i])
 #retazec[2] = 'a'    nuh uh
+
+#i = 0
+#for char in retazec:
+#    print(i,char)
+#    i +=1
+
+#for char in retazec:
+#    print(char)
+
+for i, znak in enumerate(retazec):
+    print(i, znak)
+
+
+
